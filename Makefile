c29325c0cac6a362b7dfd5b3f42a4819a0747ba3
bcoe <ben@attachments.me>
2013-04-21 22:47:47 -0700

    updating tests to be bdd.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 test:
-	@./node_modules/.bin/mocha -u tdd
+	@./node_modules/.bin/mocha -u bdd
 
 .PHONY: test
---
+++ test/rss-reader-test.js
@@ -0,0 +1,25 @@
+var assert = require('assert'),
+  RSSReader = require('../lib').RSSReader,
+  fs = require('fs');
+
+describe('RSSReader', function(done) {
+  describe('#getArticles', function() {
+    
+    it("should return an array of articles from an RSS feed", function(done) {
+      var rssreader = new RSSReader(),
+        article = fs.readFileSync('./test/fixtures/fox.rss').toString();
+
+      // Return our fixture rather than connecting to
+      // the real RSS feed.
+      rssreader._getRawFeed = function(callback) {
+        callback(null, article);
+      };
+      
+      rssreader.getArticles(function(err, articles) {
+        assert.equal(articles[0].title, "KELSEY'S LAW PUSH: Could Cellphone Companies Save Lives?");
+        done();
+      });
+    });
+
+  });
+});
