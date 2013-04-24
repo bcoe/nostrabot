767a11958a9868786c49c889f0f199be29abef4a
bcoe <ben@attachments.me>
2013-04-23 23:35:22 -0700

    Improving on unit tests for CODE article.

--- test/rss-reader-test.js
+++ test/rss-reader-test.js
@@ -1,22 +1,45 @@
 var assert = require('assert'),
   RSSReader = require('../lib').RSSReader,
+  sinon = require('sinon'),
   fs = require('fs');
 
 describe('RSSReader', function() {
-  describe('#getArticles', function() {
-    
-    it("should return an array of articles from an RSS feed", function(done) {
-      var rssreader = new RSSReader(),
-        article = fs.readFileSync('./test/fixtures/fox.rss').toString();
 
-      // Return our fixture rather than connecting to
+  describe('#readAllFeeds', function() {
+
+    it('should call #readFeed for each source in feeds', function(done) {
+      var rssreader = new RSSReader({
+        feedUrls: ['http://example.com/foo.rss', 'http://example.com/bar.rss']
+      });
+
+      // mock the readFeed method on our RSS reader using sinon.
+      rssreader.readFeed = sinon.mock().callsArgWith(1, [{}]).twice();
+
+      rssreader.readAllFeeds(function(err, articles) {
+        assert.equal(articles.length, 2);
+        done();
+      });
+
+    });
+    
+    it("should return an array of articles from a given RSS feed", function(done) {
+      var rssreader = new RSSReader({
+          feedUrls: ['http://example.com/foo.rss', 'http://example.com/bar.rss']
+        }),
+        feeds = {
+          'http://example.com/foo.rss': fs.readFileSync('./test/fixtures/fox.rss').toString(),
+          'http://example.com/bar.rss': fs.readFileSync('./test/fixtures/cnn.rss').toString()
+        };
+
+      // return our fixture rather than connecting to
       // the real RSS feed.
-      rssreader._getRawFeed = function(callback) {
-        callback(null, article);
+      rssreader._getRawFeed = function(feedUrl, callback) {
+        callback(null, feeds[feedUrl]);
       };
       
-      rssreader.getArticles(function(err, articles) {
-        assert.equal(articles[0].title, "KELSEY'S LAW PUSH: Could Cellphone Companies Save Lives?");
+      rssreader.readAllFeeds(function(err, articles) {
+        assert.equal(articles[0].title, "Arwa Damon: Freeze-frame moments");
+        assert.equal(articles[articles.length - 1].title, "Adorable Alert: Newborn Pups");
         done();
       });
     });
